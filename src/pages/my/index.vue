eb27a4a1e89357c5492dd4c7f7354bdae41b3710
duanzihuang <huangwei@itcast.cn>
2019-12-13 00:11:59 +0800

    1-完成了tabBar和navigationBar的配置

---
+++ src/pages/my/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="content">
+    <text class="title">我的---my</text>
+  </view>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {}
+  },
+  onLoad() {},
+  methods: {}
+})
+</script>
+
+<style>
+</style>
---
+++ src/pages/study/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="content">
+    <text class="title">学习---study</text>
+  </view>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {}
+  },
+  onLoad() {},
+  methods: {}
+})
+</script>
+
+<style>
+</style>
